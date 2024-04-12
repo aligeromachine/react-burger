3dce836390bbcb557525ed54ef39b262cfc4d44e
user <user@m.com>
2024-04-12 20:34:27 +0300

    fix_review_v1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+import st from './app.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={st.App}>
+      <header className={st.AppHeader}>
+        <img src={logo} className={st.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={st.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
