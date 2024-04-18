6ccf47a4ba922785490975849a7c707bef0d26e7
user <user@m.com>
2024-04-18 21:24:20 +0300

    finish_step1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from '../../logo.svg';
-import st from './app.module.css';
+import st from './app.module.css'
+import AppHeader from '../app-header/app-header';
 
 function App() {
   return (
     <div className={st.App}>
-      <header className={st.AppHeader}>
-        <img src={logo} className={st.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={st.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <main className={st.appMain}>
+        {/* <BurgerIngredients /> */}
+        {/* <BurgerConstructor /> */}
+      </main>
     </div>
   );
 }
