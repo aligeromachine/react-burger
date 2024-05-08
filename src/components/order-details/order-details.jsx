9d5531bce58f9a41bd3d28c596d532b1d6cbed03
user <user@m.com>
2024-05-09 01:05:03 +0300

    the_end_sprint2.4

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import st from './order-details.module.css';
 import orderApprove from '../../images/done.jpg';
+import { useSelector } from 'react-redux';
 
-export const OrderDetails = () => {
-  return (
+export function OrderDetails() 
+{
+	const { orderData } = useSelector(store => store.orderDetails);
+
+	return (
     <div className={st.orderContainer}>
-      <p className="text_type_digits-large">034536</p>
-      <p className="text_type_main-medium">идентификатор заказа</p>
-      <img width="120px" height="120px" alt="Заказ принят" src={orderApprove} />
-      <p className="text_type_main-default">Ваш  заказ начали готовить</p>
-      <p className="text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
+			{orderData && (
+				<>
+					<p className="text_type_digits-large">{orderData.order.number}</p>
+					<p className="text_type_main-medium">идентификатор заказа</p>
+					<img width="120px" height="120px" alt="Заказ принят" src={orderApprove} />
+					<p className="text_type_main-default">Ваш  заказ начали готовить</p>
+					<p className="text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
+				</>
+			)}      
     </div>
   );
 }
